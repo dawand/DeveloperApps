877d664aced6fd59785710b0ccb3d4db193d13ac
dawand <Su>
2018-01-10 22:52:32 +0000

    Initial Commit

---
+++ DeveloperApps/DeveloperApps.h
@@ -0,0 +1,19 @@
+//
+//  DeveloperApps.h
+//  DeveloperApps
+//
+//  Created by Dawand Sulaiman on 10/01/2018.
+//  Copyright © 2018 Kurdcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeveloperApps.
+FOUNDATION_EXPORT double DeveloperAppsVersionNumber;
+
+//! Project version string for DeveloperApps.
+FOUNDATION_EXPORT const unsigned char DeveloperAppsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeveloperApps/PublicHeader.h>
+
+
